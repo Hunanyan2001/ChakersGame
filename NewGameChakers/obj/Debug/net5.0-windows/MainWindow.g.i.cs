30429f83e56d685fc0ee050e25150b945882ee36
Hunanyan2001 <haruthunanyan10@gmail.com>
2022-05-31 20:20:20 +0400

    -----------

--- NewGameChakers/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ NewGameChakers/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace NewGameChakers {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/NewGameChakers;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/NewGameChakers;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
